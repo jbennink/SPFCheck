e324f33b2bf86964bce6b625c7bd10b1fac1e2bf
Johan Bennink <j.bennink@calway.nl>
2024-07-29 17:09:58 +0200

    Adopt Spectre.Console (but not the CLI parsing where McMaster is better in my opinion

--- src/SPFCheck/Program.cs
+++ src/SPFCheck/Program.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using DnsClient;
 using DnsClient.Protocol;
 using McMaster.Extensions.CommandLineUtils;
+using Spectre.Console;
 
 namespace SPFCheck
 {
@@ -26,17 +27,41 @@ namespace SPFCheck
                 return null;
             }
 
+            (bool found, string message) CheckSPF(string domain)
+            {
+                var domainT = domain.TrimEnd();
+                var lookup = new LookupClient();
+                var TxtInfo = lookup.Query(domainT, QueryType.TXT).Answers.OfType<TxtRecord>().Where(r => r.Text.FirstOrDefault().Contains("spf1"));
+                switch (TxtInfo.Count())
+                {
+                    case 0:
+                        return (false, "no SPF records found on domain!!!");
+                    case 1:
+                        var info = TxtInfo.First();
+                        string txt = info.Text.FirstOrDefault();
+                        string spf = DomainContainsSPF(txt);
+                        if (spf != null)
+                        {
+                            return (true, $"[green]SPF found[/]: [bold]{spf}[/]");
+                        }
+                        else
+                        {
+                            return (false, $"[red]SPF missing[/]: {txt}");
+                        }
+                    default:
+                        return (false, "more than 1 SPF found on domain, bad config!!!");
+                };
+            }
             var app = new CommandLineApplication();
             app.Name = nameof(SPFCheck);
             app.ResponseFileHandling = ResponseFileHandling.ParseArgsAsSpaceSeparated;
             app.HelpOption();
 
             var optionFileSource = app.Option("-f|--file <filepath>", "a file containing 1 domain per line to process", CommandOptionType.SingleOrNoValue);
-            var optionDomain = app.Option("-d|--domain <domain>", "a domain to process", CommandOptionType.SingleOrNoValue);
+            var optionDomain = app.Option("-d|--domain <domain>", "a domain to process", CommandOptionType.MultipleValue);
             var optionSpf = app.Option("--spf <spfmatch>", "a domain or dns to match in the spf for the given domain(s)", CommandOptionType.MultipleValue);
             var optionVersion = app.Option("--version", "display version number", CommandOptionType.NoValue);
             app.OnExecute(() => {
-
                 if(optionVersion.HasValue())
                 {
                     var assembly = Assembly.GetExecutingAssembly();
@@ -50,49 +75,53 @@ namespace SPFCheck
                 }
                 if(optionDomain.HasValue())
                 {
-                    domainList.Add(optionDomain.Value());
+                    domainList.AddRange(optionDomain.Values);
                 }
                 if (optionSpf.HasValue())
                 {
                     checkFor.AddRange(optionSpf.Values);
                 }
                 if(domainList.Count == 0 || checkFor.Count==0) {
-                    Console.WriteLine("No or empty list of domains of spf supplied");
+                    AnsiConsole.MarkupLine("No or empty list of domains of spf supplied");
                     return;
                 }
-
-                foreach (string domain in domainList)
+                if (domainList.Count == 1)
                 {
-                    var domainT = domain.TrimEnd();
-                    Console.Write($"Check domain: {domainT}: ");
-                    var lookup = new LookupClient();
-                    var TxtInfo = lookup.Query(domainT, QueryType.TXT).Answers.OfType<TxtRecord>().Where(r => r.Text.FirstOrDefault().Contains("spf1"));
-                    switch (TxtInfo.Count())
+                    // Single
+                    var domainT = domainList.First().TrimEnd();
+                    (bool status, string message) = CheckSPF(domainT);
+                    AnsiConsole.Markup($"Domain: [bold]{domainT}[/] -- Status: ");
+                    if(status)
                     {
-                        case 0:
-                            Console.WriteLine("no SPF records found on domain!!!");
-                            break;
-                        case 1:
-                            foreach (var info in TxtInfo)
-                            {
-                                string txt = info.Text.FirstOrDefault();
-                                string spf = DomainContainsSPF(txt);
-                                if (spf != null)
-                                {
-                                    Console.WriteLine($"SPF found: {spf}");
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"missing record in SPF: {txt}");
-                                }
-                            }
-                            break;
-                        default:
-                            Console.WriteLine("more than 1 SPF found on domain, bad config!!!");
-                            break;
-                    };
-                    Console.WriteLine();
-                }               
+                        AnsiConsole.MarkupLine(message);
+                    }
+                    else
+                    {
+                        AnsiConsole.MarkupLine(message);
+                    }
+                    AnsiConsole.WriteLine();
+                }
+                else
+                {
+                    var table = new Table();
+                    table.AddColumn(new TableColumn(new Markup("Domain")));
+                    table.AddColumn(new TableColumn(new Markup("Status")));
+                    table.AddColumn(new TableColumn(new Markup("Error")));
+                    foreach (string domain in domainList)
+                    {
+                        var domainT = domain.TrimEnd();
+                        (bool status, string message) = CheckSPF(domainT);
+                        if(status)
+                        {
+                            table.AddRow(new Markup(domainT), new Markup("[green]SPF found[/]"), new Markup(""));
+                        }
+                        else
+                        {
+                            table.AddRow(new Markup(domainT), new Markup("[red]error[/]"), new Markup(message));
+                        }
+                    }
+                    AnsiConsole.Write(table);
+                }
             });
             try
             {
@@ -100,7 +129,7 @@ namespace SPFCheck
             }
             catch(UnrecognizedCommandParsingException ex)
             {
-                Console.WriteLine($"Error in commandline parameters: {ex.Message}");
+                AnsiConsole.WriteLine($"Error in commandline parameters: {ex.Message}");
             }
             return -1;
         }
